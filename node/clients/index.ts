6f3245665a9001a91a1a9f115c169c85347817d4
Bruno Monteiro <brunobapmon@gmail.com>
2023-05-31 14:29:46 -0300

    feat: oms client

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,9 +1,15 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { IOClients } from '@vtex/api'
+import { OMS } from '@vtex/clients'
 
 import { Acquirer } from './acquirer'
 
 export class Clients extends IOClients {
   public get acquirer() {
     return this.getOrSet('acquirer', Acquirer)
+  }
+
+  public get oms() {
+    return this.getOrSet('oms', OMS)
   }
 }
