30870990e1321590fd982d453f1acff3349cf516
Bruno Monteiro <brunobapmon@gmail.com>
2023-05-31 14:29:30 -0300

    feat: supplier provider middleware

---
+++ node/logic/suppliers.ts
@@ -0,0 +1,84 @@
+import type { IAffiliate, RecipientsBuilderPayload, Supplier } from '../typings'
+import { getAffiliateByIdLogic } from './getAffiliateById'
+import { getAffiliateCodeLogic } from './getAffiliateCode'
+
+interface ICustomApp {
+  id: string
+  fields: Record<string, unknown>
+}
+
+export async function getSuppliersByMiniCart(
+  payload: RecipientsBuilderPayload,
+  ctx: Context
+): Promise<Supplier[]> {
+  const order = await ctx.clients.oms.order(payload.orderId)
+
+  if (!order.customData || !payload.operationValue) {
+    return []
+  }
+
+  const affiliatesCustomData = order.customData.customApps.find(
+    (app: ICustomApp) => app.id === 'avanti-vtexio-boilerplate-backend'
+  )
+
+  if (!affiliatesCustomData) {
+    return []
+  }
+
+  const { code } = affiliatesCustomData.fields
+
+  const searchCode = await getAffiliateCodeLogic(code, ctx)
+
+  if (!searchCode) {
+    return []
+  }
+
+  const searchAffiliate = await getAffiliateByIdLogic(
+    searchCode.affiliateId,
+    ctx
+  )
+
+  const { affiliateId, cpf, name, sponsor } = searchAffiliate
+
+  let affiliateSponsor: IAffiliate | undefined
+
+  if (sponsor) {
+    affiliateSponsor = await getAffiliateByIdLogic(sponsor.affiliateId, ctx)
+  }
+
+  const affiliateCommision = affiliateSponsor
+    ? payload.operationValue * 0.3
+    : payload.operationValue * 0.285
+
+  const sponsorCommision = affiliateSponsor ? payload.operationValue * 0.015 : 0
+
+  const suppliers = [
+    {
+      id: affiliateId,
+      name,
+      amount: affiliateCommision,
+      document: cpf,
+      documentType: 'CPF',
+      role: 'affiliate',
+      commissionAmount: 0,
+      chargebackLiable: false,
+      chargeProcesssingFee: false,
+    },
+  ]
+
+  if (affiliateSponsor) {
+    suppliers.push({
+      id: affiliateSponsor.affiliateId,
+      name: affiliateSponsor.name,
+      amount: sponsorCommision,
+      document: affiliateSponsor.cpf,
+      documentType: 'CPF',
+      role: 'affiliate',
+      commissionAmount: 0,
+      chargebackLiable: false,
+      chargeProcesssingFee: false,
+    })
+  }
+
+  return suppliers
+}
