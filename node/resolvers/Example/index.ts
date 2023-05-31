28702aedc484525450256d7285f01f91c3cece72
jacksonvfo <jackson.oliveira@penseavanti.com.br>
2023-05-31 16:43:19 -0300

    feat: add supplier form

--- node/resolvers/Example/index.ts
+++ node/resolvers/Example/index.ts
@@ -1,7 +1,7 @@
-export async function example (
+export async function example(
   _: any,
   _props: any,
-  { clients: { example: exampleClient } }: Context
+  { clients: { example: exampleClient } }: Context,
 ) {
   const resp: any = await exampleClient.getExample()
   const items = await resp.data.map((data: { item: string }) => data.item)
