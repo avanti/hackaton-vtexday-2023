3c379a6dd35fdaa7aa3c25bc54bca5fb9a309c16
Bruno Monteiro <brunobapmon@gmail.com>
2023-06-02 17:06:31 -0300

    feat: checkout initial files

---
+++ checkout-ui-custom/checkout6-custom.js
@@ -0,0 +1,86 @@
+const addAffiliateSupplierCodeField = (ownersNames) => {
+  const affiliateCodeFieldWrapper = document.createElement('div')
+  affiliateCodeFieldWrapper.id = 'affiliateCodeFieldWrapper'
+
+  const addCodeButton = document.createElement('button')
+  addCodeButton.innerText = 'Adicionar código'
+
+  affiliateCodeFieldWrapper.appendChild(addCodeButton)
+
+  document
+    .querySelector('.cart-totalizers')
+    .appendChild(affiliateCodeFieldWrapper)
+
+  const addInputField = () => {
+    const codeField = document.createElement('input')
+    codeField.placeholder = 'Código do afiliado'
+
+    const addButton = document.createElement('button')
+    addButton.innerText = 'Adicionar'
+
+    const codeText = document.createElement('span')
+
+    const removeButton = document.createElement('button')
+    removeButton.innerText = 'Remover'
+    removeButton.style.display = 'none'
+
+    affiliateCodeFieldWrapper.appendChild(codeField)
+    affiliateCodeFieldWrapper.appendChild(addButton)
+    affiliateCodeFieldWrapper.appendChild(codeText)
+    affiliateCodeFieldWrapper.appendChild(removeButton)
+
+    addCodeButton.style.display = 'none'
+    codeField.style.display = 'block'
+    addButton.style.display = 'block'
+
+    const updateDisplay = () => {
+      if (codeField.style.display === 'none') {
+        codeField.style.display = 'block'
+        addButton.style.display = 'block'
+        codeText.style.display = 'none'
+        removeButton.style.display = 'none'
+      } else {
+        codeField.style.display = 'none'
+        addButton.style.display = 'none'
+        codeText.style.display = 'block'
+        removeButton.style.display = 'block'
+        codeText.innerText = codeField.value
+      }
+    }
+
+    addButton.addEventListener('click', () => {
+      updateDisplay()
+      removeButton.style.display = 'block'
+    })
+
+    removeButton.addEventListener('click', () => {
+      affiliateCodeFieldWrapper.removeChild(codeField)
+      affiliateCodeFieldWrapper.removeChild(addButton)
+      affiliateCodeFieldWrapper.removeChild(codeText)
+      affiliateCodeFieldWrapper.removeChild(removeButton)
+      addCodeButton.style.display = 'block'
+    })
+  }
+
+  addCodeButton.addEventListener('click', addInputField)
+
+  const cartToOrderFormButton = document.getElementById('cart-to-orderform')
+
+  cartToOrderFormButton.addEventListener('click', () => {
+    const codeField = document.querySelector('#affiliateCodeFieldWrapper input')
+    if (codeField && codeField.value) {
+      return vtexjs.checkout.setCustomData({
+        app: 'affiliates-program',
+        field: 'affiliateCode',
+        value: codeField.value,
+      })
+    }
+  })
+}
+
+let intervalId = setInterval(function () {
+  if (typeof vtexjs !== 'undefined') {
+    clearInterval(intervalId)
+    addAffiliateSupplierCodeField()
+  }
+}, 100)
