fb860d49a5247f6c7d1e84eb69c848547cd08c2c
Bruno Monteiro <brunobapmon@gmail.com>
2023-06-06 12:18:03 -0300

    feat: script and css

--- checkout-ui-custom/checkout6-custom.js
+++ checkout-ui-custom/checkout6-custom.js
@@ -1,15 +1,18 @@
-const addAffiliateSupplierCodeField = (ownersNames) => {
+const addAffiliateSupplierCodeField = async () => {
   const affiliateCodeFieldWrapper = document.createElement('div')
   affiliateCodeFieldWrapper.id = 'affiliateCodeFieldWrapper'
 
   const addCodeButton = document.createElement('button')
-  addCodeButton.innerText = 'Adicionar código'
+  addCodeButton.innerText = 'Adicionar código de afiliado'
 
   affiliateCodeFieldWrapper.appendChild(addCodeButton)
 
-  document
-    .querySelector('.cart-totalizers')
-    .appendChild(affiliateCodeFieldWrapper)
+  const cartTotalizers = document.querySelector('.cart-totalizers')
+
+  cartTotalizers.insertBefore(
+    affiliateCodeFieldWrapper,
+    cartTotalizers.childNodes[2]
+  )
 
   const addInputField = () => {
     const codeField = document.createElement('input')
@@ -80,7 +83,62 @@ const addAffiliateSupplierCodeField = (ownersNames) => {
     })
   }
 
+  const addInputFieldWithCodeFromOrderForm = async (code) => {
+    const codeField = document.createElement('input')
+    codeField.placeholder = 'Código do afiliado'
+
+    const addButton = document.createElement('button')
+    addButton.innerText = 'Adicionar'
+
+    const codeText = document.createElement('span')
+
+    const removeButton = document.createElement('button')
+    removeButton.innerText = 'Remover'
+    removeButton.style.display = 'none'
+
+    affiliateCodeFieldWrapper.appendChild(codeField)
+    affiliateCodeFieldWrapper.appendChild(addButton)
+    affiliateCodeFieldWrapper.appendChild(codeText)
+    affiliateCodeFieldWrapper.appendChild(removeButton)
+
+    addCodeButton.style.display = 'none'
+    codeField.style.display = 'block'
+    addButton.style.display = 'block'
+
+    codeField.style.display = 'none'
+    addButton.style.display = 'none'
+    codeText.style.display = 'block'
+    removeButton.style.display = 'block'
+    codeText.innerText = code
+
+    await vtexjs.checkout.setCustomData({
+      app: 'affiliates-program',
+      field: 'affiliateCode',
+      value: code,
+    })
+
+    removeButton.addEventListener('click', () => {
+      affiliateCodeFieldWrapper.removeChild(codeField)
+      affiliateCodeFieldWrapper.removeChild(addButton)
+      affiliateCodeFieldWrapper.removeChild(codeText)
+      affiliateCodeFieldWrapper.removeChild(removeButton)
+      // we need this fetch because vtexjs does not have a method to remove customData
+      fetch(
+        `/api/checkout/pub/orderForm/228fae38fbcd430ca6954c15e4076931/customData/affiliates-program/affiliateCode`,
+        {
+          headers: {},
+          method: 'DELETE',
+        }
+      )
+      addCodeButton.style.display = 'block'
+    })
+  }
+
   addCodeButton.addEventListener('click', addInputField)
+  const orderForm = await vtexjs.checkout.getOrderForm()
+  if (orderForm.marketingData?.utmSource) {
+    addInputFieldWithCodeFromOrderForm(orderForm.marketingData?.utmSource)
+  }
 }
 
 let intervalId = setInterval(function () {
